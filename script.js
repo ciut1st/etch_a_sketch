55fd2f2baa6ca0df90916520d52e740025c5d84f
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-08 16:20:56 +1100

    preparation

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const div = document.querySelector('.container')
+let board = document.querySelector('.board')
+board.style.gridTemplateColumns = 'repeat(16, 1fr)'
+board.style.gridTemplateRows = 'repeat(16, 1fr)'
 
-const box = document.createElement('div')
-box.style.border = 'black'
-box.style.backgroundColor = 'pink'
-box.textContent = "hi"
-
-div.appendChild(box)
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div')
+    square.style.backgroundColor = 'blue'
+    board.insertAdjacentElement('beforeend', square)
+}

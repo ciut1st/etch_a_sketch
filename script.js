a1b9df8089b05eb62203b3f6d79092189416d824
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-09 14:32:44 +1100

    done styling for now

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let board = document.querySelector('.board')
 board.style.gridTemplateColumns = 'repeat(16, 1fr)'
 board.style.gridTemplateRows = 'repeat(16, 1fr)'
+let squares =  document.querySelectorAll('div')
 
 for (let i = 0; i < 256; i++) {
     let square = document.createElement('div')
     square.style.backgroundColor = 'blue'
     board.insertAdjacentElement('beforeend', square)
 }
+
+function resetButton() {
+    const resetBtn = document.querySelector('.reset')
+    let squares = document.querySelectorAll('div')
+    resetBtn.addEventListener('click', () => {
+        squares.forEach((div) => {
+            div.style.backgroundColor = 'white'
+        })
+    })
+}
+console.log(resetButton())

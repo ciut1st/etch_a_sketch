4cc2036dd400bb191f9ca284453c5ba24a4ba2d7
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-09 17:41:53 +1100

    there is a bug in the user prompt

--- script.js
+++ script.js
@@ -5,11 +5,25 @@ board.style.gridTemplateRows = 'repeat(16, 1fr)'
 for (let i = 0; i < 256; i++) {
     let square = document.createElement('div')
     square.classList.add('cell')
-    square.style.backgroundColor = 'grey'
+    square.style.backgroundColor = 'white'
     board.insertAdjacentElement('beforeend', square)
 }
 
-// Black button color
+function userSize(input) {
+    let newSize = document.querySelector('.size')
+    newSize.addEventListener('click', () => {
+        prompt("Please enter a new size: (min: 2, max: 100)")
+        if (input >= 2 && input <= 100) {
+            alert("yes")
+        } else {
+            alert("Please pick a number between 2 and 100")
+        }
+    })
+}
+
+console.log(userSize())
+
+// Black button color--------------------------------------
 function blackColorButton() {
     const blackColorBtn = document.querySelector('.black')
     let squares = document.querySelectorAll('.cell')
@@ -39,31 +53,32 @@ function whiteColorButton() {
 console.log(whiteColorButton())
 // -------------------------------------------------------------
 
-// Reset button ------------------------------------------
-function resetButton() {
-    const resetBtn = document.querySelector('.reset')
-    let squares = document.querySelectorAll('div')
-    resetBtn.addEventListener('click', () => {
-        squares.forEach((div) => {
-            div.style.backgroundColor = 'white'
-        })
-    })
-}
-console.log(resetButton())
-// -------------------------------------------------------
-
 // Random color ----------------------------------------------------
 function randomColor() {
     const randomBtn = document.querySelector('.random')
-    let squares = document.querySelectorAll('div')
+    let squares = document.querySelectorAll('.cell')
     let random = Math.floor(Math.random() * 16777215).toString(16)
     randomBtn.addEventListener('click', () => {
-        squares.forEach((div) => {
-            div.addEventListener('mouseover', () => {
-                div.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        squares.forEach((cell) => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
             })
         })
     })
 }
 console.log(randomColor())
 //--------------------------------------------------------------------
+
+// Reset button ------------------------------------------
+function resetButton() {
+    const resetBtn = document.querySelector('.reset')
+    let squares = document.querySelectorAll('.cell')
+    resetBtn.addEventListener('click', () => {
+        squares.forEach((cell) => {
+            cell.style.backgroundColor = 'white'
+        })
+    })
+}
+console.log(resetButton())
+// -------------------------------------------------------
+
